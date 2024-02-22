ccf2bf4fd78fc800ccc8bb4868e538f9f7f1f3cd
RItik <reallyhritik@gmail.com>
2024-02-22 18:03:15 +0530

    new commit by really hritik

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('reallyhritikroute', function () {
+    return view('platform.layout');
+});
 
 
 Route::get('/', function () {
